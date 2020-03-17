4a6312b1167aeb27bc4d62b87acf47d97e7aa01a
Robert Moel <58125997+randrmoel@users.noreply.github.com>
2020-03-17 17:53:38 -0400

    Type fix

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,7 +16,7 @@ module.exports = function(app){
         });
     });
 
-    // Write a new node, grab a new ID from uniqid
+    // Write a new note, grab a new ID from uniqid
     app.post("/api/notes", function(req, res){
         let newID = uniqid();
         //Create the object
